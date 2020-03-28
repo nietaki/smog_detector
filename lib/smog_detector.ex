feb3e017b7038502622ce8be7f91ed4620d745e5
Jacek Królikowski <nietaki@gmail.com>
2020-03-28 10:54:58 +0100

    initial commit

---
+++ lib/smog_detector.ex
@@ -0,0 +1,18 @@
+defmodule SmogDetector do
+  @moduledoc """
+  Documentation for SmogDetector.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SmogDetector.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/smog_detector_test.exs
@@ -0,0 +1,8 @@
+defmodule SmogDetectorTest do
+  use ExUnit.Case
+  doctest SmogDetector
+
+  test "greets the world" do
+    assert SmogDetector.hello() == :world
+  end
+end
